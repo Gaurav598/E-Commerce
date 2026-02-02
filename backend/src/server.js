e6b301f5d829b8f47c201d70d59a0204a4ddd268
GAURAV KUMAR THAKUR <gr598895@gmail.com>
2026-02-03 03:24:42 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -55,13 +55,13 @@ app.get("/api/health", (req, res) => {
 });
 
 // make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../admin/dist")));
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-  });
-}
+//   app.get("/{*any}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+//   });
+// }
 
 const startServer = async () => {
   await connectDB();
